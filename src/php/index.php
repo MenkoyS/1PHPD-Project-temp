798bfc157e49f9c815bc2a86ed98d6ae49cc5810
MenkoyS <yann.hobbe@supinfo.com>
2024-04-19 03:31:13 +0200

    feature(login-page) : front of register and login page are done

---
+++ src/php/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="left-side">
+            <div class="form-container">
+                <h2>Register</h2>
+                <form>
+                    <input type="text" placeholder="Username">
+                    <input type="password" placeholder="Password">
+                    <input type="password" placeholder="Confirm Password">
+                    <input type="email" placeholder="Email">
+                    <input type="submit" value="Register">
+                </form>
+            </div>
+        </div>
+        <div class="right-side">
+            <div class="form-container">
+                <h2>Login</h2>
+                <form>
+                    <input type="text" placeholder="Username">
+                    <input type="password" placeholder="Password">
+                    <input type="submit" value="Login">
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
