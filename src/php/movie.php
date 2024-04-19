a03fb04499fcb1f6207b2ceecf97ce85e9c091dd
CoisneVictor <victor.coisne@supinfo.com>
2024-04-19 11:22:20 +0200

    feature(front-movie-page): front of the movie page globally done + new assets

---
+++ src/php/movie.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/movie.css">
+    <title>Weeklymotion</title>
+</head>
+<body>
+    <div>
+        <h1>Mise en avant</h1>
+        <h2>Insérer le nom de la vidéo</h2>
+        <div class="putForward">
+            <div class="txtPutForward">
+                <h3>Description de la vidéo</h3>
+            </div>
+            <h3 class="trailerBox">Dans la trailer box</h3>
+        </div>
+    </div>
+    <div class="space">
+        <h1>Action</h1>
+        <div class="videoBox">
+            <div class ="boxInsideImg">
+                <img src="../../assets/logo.png" draggable="False" alt="Film image">   
+                <div class="boxInsideText">
+                    <h3>Title</h3>
+                    <p>Description</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="space">
+        <h1>Drama</h1>
+        <div class="videoBox">
+            <div class ="boxInsideImg">
+                <img src="../../assets/logo.png" draggable="False" alt="Film image">   
+                <div class="boxInsideText">
+                    <h3>Title</h3>
+                    <p>Description</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<footer>
+    <div class="imgFooter">
+        <a href="https://youtube.com" target="_blank"><img src="../../assets/youtube.png" alt="Youtube Weeklymotion"></a>
+        <a href="https://instagram.com" target="_blank"><img src="../../assets/insta.png" alt="Instagram Weeklymotion"></a>
+        <a href="https://facebook.com" target="_blank"><img src="../../assets/facebook.png" alt="Facebook Weeklymotion"></a>
+        <a href="https://twitter.com" target="_blank"><img src="../../assets/twitter.png" alt="Twitter Weeklymotion"></a>
+    </div>
+    <div class="txtFooter">
+        <p>© 2024 Weeklymotion</p>
+    </div>
+</footer>
+</html>
