4936a7c2e2ccbe750a3565be523ebd3134a52f87
CoisneVictor <victor.coisne@supinfo.com>
2024-04-20 14:31:07 +0200

    feature(movie-info): front of the page almost done

---
+++ src/php/movie-info.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/movie-info.css">
+    <title>Weeklymotion - Movie Information</title>
+</head>
+
+<body>
+    <main>
+        <div class="infoSide">
+            <h1>Movie Title</h1>
+            <h2>Actors :</h2>
+            <h3>Emily Blunt</h3>
+            <h2>Price : 14 $</h2>
+            <h2>Rating : 4 stars</h2>
+            <h2>Time : 2h55min</h2>
+            <div class="divcenter">
+                <button>
+                    <span class="transition"></span>
+                    <span class="gradient"></span>
+                    <span class="label">Add to cart</span>
+                </button>
+            </div>
+            <div>
+                <h2>Director's others films :</h2>
+                <div class="filmBox">
+                    <img class="filmBoxImg" src="../../assets/oppenheimer.jpg" alt="Film mit en avant" draggable="False">
+                    <p class="filmBoxText">Movie title</p>
+                </div>
+            </div>
+        </div>
+        <img src="../../assets/oppenheimer.jpg" alt="Film poster" draggable="False">
+    </main>
+    <footer>
+        <div class="imgFooter">
+            <a href="https://youtube.com" target="_blank"><img src="../../assets/youtube.png"
+                    alt="Youtube Weeklymotion"></a>
+            <a href="https://instagram.com" target="_blank"><img src="../../assets/insta.png"
+                    alt="Instagram Weeklymotion"></a>
+            <a href="https://facebook.com" target="_blank"><img src="../../assets/facebook.png"
+                    alt="Facebook Weeklymotion"></a>
+            <a href="https://twitter.com" target="_blank"><img src="../../assets/twitter.png"
+                    alt="Twitter Weeklymotion"></a>
+        </div>
+        <div class="txtFooter">
+            <p>© 2024 Weeklymotion</p>
+        </div>
+    </footer>
+</body>
+
+</html>
