523dc8f7a57f3a9f0223eb135ea07529f7fed1d5
MenkoyS <yann.hobbe@supinfo.com>
2024-04-24 20:10:56 +0200

    feature(login) : implementation of togglePasswordVisibility to see the pwds

--- website/src/index.php
+++ website/src/index.php
@@ -23,6 +23,24 @@
                     <input type="submit" value="Register">
                 </form>
 
+
+                <script>
+
+                    function togglePasswordVisibility() {
+                        var passwordField = document.getElementById("password");
+                        var confirmPasswordField = document.getElementById("confirmPassword");
+                        var showPasswordCheckbox = document.getElementById("showPassword");
+
+                        if (showPasswordCheckbox.checked) {
+                            passwordField.type = "text";
+                            confirmPasswordField.type = "text";
+                        } else {
+                            passwordField.type = "password";
+                            confirmPasswordField.type = "password";
+                        }
+                    }
+                </script>
+
             </div>
         </div>
         <div class="right-side">
