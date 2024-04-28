2f11cc2f51f59e52e3b0ed9b003d7fd2431e8dc8
CoisneVictor <victor.coisne@supinfo.com>
2024-04-28 20:13:04 +0200

    test/test-api Added details

--- website/src/test_basket.php
+++ website/src/test_basket.php
@@ -32,12 +32,13 @@ try{
     <h1>Your basket</h1>
     <div class="boxElement">
     <?php 
-    if ($film) {
-            echo "<img src='" . $film['image_link'] . "' alt='Image du film' style='max-width: autopx; max-height: 200px;'>";
-        }
+    if ($film) {echo "<img src='" . $film['image_link'] . "' alt='Image du film' style='max-width: autopx; max-height: 200px;'>";}
     ?>
         <div class="paddingSpace">
-        <h2>Titre</h2>
+        <h2><?php if($film){echo $film['title'];}?></h2>
+        <h3>Director : <?php if($film){echo $film['director'];}?></h3>
+        <h3>Genre : <?php if($film){echo $film['genre'];}?></h3>
+        <h3>Price : <?php if($film){echo $film['price'];}?></h3>
         </div>
     </div>
     <div class="buttonPosition">
@@ -49,7 +50,7 @@ try{
         <button>
             <span class="transition2"></span>
             <span class="gradient"></span>
-            <span class="label">Delete</span>
+            <span class="label">Delete All</span>
         </button>
     </div>
 </body>
