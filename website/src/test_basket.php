56dfdd2bb83a597daa1b0b6d17af77cf66675912
CoisneVictor <victor.coisne@supinfo.com>
2024-04-28 11:44:10 +0200

    test/test-api added test_basket.php and basket.css

---
+++ website/src/test_basket.php
@@ -0,0 +1,56 @@
+<?php
+$host = 'db'; 
+$dbname = 'my_db';
+$username = 'my_user'; 
+$password = 'my_password';
+
+try{
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $filmId = 1;
+
+    $stmt = $pdo->prepare("SELECT * FROM film WHERE id = :filmId");
+    $stmt->bindParam(':filmId', $filmId);
+    $stmt->execute();
+    $film = $stmt->fetch(PDO::FETCH_ASSOC);
+
+} catch (PDOException $e) {
+    echo "Erreur lors de la récupération du film : " . $e->getMessage();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/basket.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Your basket</h1>
+    <div class="boxElement">
+    <?php 
+    if ($film) {
+            echo "<img src='" . $film['image_link'] . "' alt='Image du film' style='max-width: autopx; max-height: 200px;'>";
+        }
+    ?>
+        <div class="paddingSpace">
+        <h2>Titre</h2>
+        </div>
+    </div>
+    <div class="buttonPosition">
+        <button>
+            <span class="transition"></span>
+            <span class="gradient"></span>
+            <span class="label">Buy</span>
+        </button>
+        <button>
+            <span class="transition2"></span>
+            <span class="gradient"></span>
+            <span class="label">Delete</span>
+        </button>
+    </div>
+</body>
+</html>
